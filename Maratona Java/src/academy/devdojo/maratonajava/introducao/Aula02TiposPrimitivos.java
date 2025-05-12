d839c10a71f37265b5e5e171a4c6c8fcbeff6a0d
Thiago Rocha <thiagotlgdmn@gmail.com>
2025-05-12 17:56:08 -0300

    Agora vai

---
+++ Maratona Java/src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = -128;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 83;
+        String nome = "DD";
+
+        System.out.println("A idade é "+idade+" anos");
+        System.out.println(caractere);
+        System.out.println("O melhor cachorro dos jogos é o "+nome);
+    }
+}
---
+++ Maratona Java/src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Thiago";
+        String endereco = "Rua dos tupis 24";
+        double salario = 2200;
+        String dataPagamento = "12 de maio";
+
+        System.out.println("Eu "+nome+", morando no "+endereco+" confirmo que recebi o salario de "+salario+", na data "+dataPagamento);
+    }
+}
+
