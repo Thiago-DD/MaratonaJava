7741be232ecfbc2f5e8925fecbbbd72e4a4babae
Thiago Rocha <thiagotlgdmn@gmail.com>
2025-06-02 17:30:30 -0300

    mais um dia

--- Maratona Java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ Maratona Java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -14,4 +14,26 @@ public class Calculadora {
         System.out.println(num1 * num2);
     }
 
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public void imprimedivideDoisNumeros(double num1, double num2) {
+        if (num2 != 0) {
+            System.out.println(num1 / num2);
+        } else {
+            System.out.println("Não existe divisão por zero");
+        }
+    }
+
+    public void AlterarDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do AlteraDoisNumeros");
+        System.out.println("Num1 " + num1);
+        System.out.println("Num2 " + num2);
+    }
 }
