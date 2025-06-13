e6cdb291f8ff6162c35eaeb3ff5a7347523d093d
Thiago <thiagotlgdmn@gmail.com>
2025-06-13 15:44:38 -0300

    tá complicado

--- Maratona Java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
+++ Maratona Java/src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -1,13 +1,14 @@
-package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
 
 public class Anime {
     private String nome;
-    private int[] episodeos;
+    private static int[] episodeos;
+    // 0 - Bloco de inicialização é executado quando a JVM carrega classes
     // 1 - alocado espaço em memoria pro objeto
     // 2 - cada atributo de classe é criado e inicializado com valores defoult ou o que for passada
     // 3 - Bloco de inicialização é executado
     // 4 - construtor é executado
-    {
+    static {
         System.out.println("Dentro do bloco de inicialização");
         episodeos = new int[100];
         for (int i = 0; i < episodeos.length; i++) {
@@ -15,13 +16,13 @@ public class Anime {
         }
     }
 
-    public Anime(String nome){
+    public Anime(String nome) {
         this.nome = nome;
     }
 
     public Anime() {
 
-        for (int episodeos : this.episodeos) {
+        for (int episodeos : Anime.episodeos) {
             System.out.print(episodeos + " ");
         }
         System.out.println();
